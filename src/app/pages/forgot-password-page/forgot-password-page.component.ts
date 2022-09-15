a0780ef0613579b7b494f437ee51a7adce13eb92
Roman Savran <roblumination@gmail.com>
2022-09-15 20:03:18 +0300

    finish with reset password page

--- src/app/pages/forgot-password-page/forgot-password-page.component.ts
+++ src/app/pages/forgot-password-page/forgot-password-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password-page',
@@ -6,14 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password-page.component.scss'],
 })
 export class ForgotPasswordPageComponent {
+  isNextState = false;
   text = 'Enter your email from registered account';
+  emailForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+  });
+
+  get email() {
+    return this.emailForm.get('email');
+  }
+
+  onSumbit() {
+    if (this.emailForm.valid) {
+      this.nextStep();
+    }
+  }
 
   nextStep() {
+    this.isNextState = true;
     this.text =
       'Link to change your password has been sent to provided email if we have it inside our system';
   }
 
   reset() {
+    this.isNextState = false;
     this.text = 'Enter your email from registered account';
   }
 }
